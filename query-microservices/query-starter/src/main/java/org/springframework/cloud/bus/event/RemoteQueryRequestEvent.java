435f8a6abdf00460aead884e38820a8cfd8f4865
Whitney O'Meara <james.w.omeara@gmail.com>
2021-05-18 16:41:20 -0400

    Various updates:  - Updated Query Json Serialization to be polymorphic.  - Updated Query Json Serialization to serialize the query as a string.  - Created a ProxiedUserEntity-specific AuthorizationsUtil to avoid usage of DatawavePrincipal.  - Updated all query microservices to use the spring-boot provided log4j dependencies.  - Moved Local query storage cache implementation to test and created a test jar.  - Renamed the query cache dependency to query-storage to be consistent with the api and parent modules.  - Renamed various application launcher classes to be consistent with our existing microservices.  - Removed Autowired injection for components where a constructor would suffice.  - Updated QueryTaskNotification to have it be published via the spring cloud bus.  - Many other various changes/improvements.

--- query-microservices/query-starter/src/main/java/org/springframework/cloud/bus/event/RemoteQueryRequestEvent.java
+++ query-microservices/query-starter/src/main/java/org/springframework/cloud/bus/event/RemoteQueryRequestEvent.java
@@ -2,21 +2,21 @@ package org.springframework.cloud.bus.event;
 
 import datawave.microservice.query.remote.QueryRequest;
 
-public class QueryRemoteRequestEvent extends RemoteApplicationEvent {
+public class RemoteQueryRequestEvent extends RemoteApplicationEvent {
     
     private final QueryRequest queryRequest;
     
     @SuppressWarnings("unused")
-    public QueryRemoteRequestEvent() {
+    public RemoteQueryRequestEvent() {
         // this constructor is only for serialization/deserialization
         queryRequest = null;
     }
     
-    public QueryRemoteRequestEvent(Object source, String originService, QueryRequest queryRequest) {
+    public RemoteQueryRequestEvent(Object source, String originService, QueryRequest queryRequest) {
         this(source, originService, null, queryRequest);
     }
     
-    public QueryRemoteRequestEvent(Object source, String originService, String destinationService, QueryRequest queryRequest) {
+    public RemoteQueryRequestEvent(Object source, String originService, String destinationService, QueryRequest queryRequest) {
         super(source, originService, DEFAULT_DESTINATION_FACTORY.getDestination(destinationService));
         this.queryRequest = queryRequest;
     }
