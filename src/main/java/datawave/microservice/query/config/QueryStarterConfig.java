9137cabf688a3d9e77a5813830865d9804ee7be8
Whitney O'Meara <james.w.omeara@gmail.com>
2022-08-18 12:01:36 -0400

    removed missing reference

--- src/main/java/datawave/microservice/query/config/QueryStarterConfig.java
+++ src/main/java/datawave/microservice/query/config/QueryStarterConfig.java
@@ -1,11 +1,10 @@
 package datawave.microservice.query.config;
 
 import datawave.microservice.query.edge.config.EdgeDictionaryProviderProperties;
-import datawave.microservice.query.mapreduce.config.MapReduceQueryProperties;
 import datawave.microservice.query.stream.StreamingProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties({QueryProperties.class, MapReduceQueryProperties.class, StreamingProperties.class, EdgeDictionaryProviderProperties.class})
+@EnableConfigurationProperties({QueryProperties.class, StreamingProperties.class, EdgeDictionaryProviderProperties.class})
 public class QueryStarterConfig {}
