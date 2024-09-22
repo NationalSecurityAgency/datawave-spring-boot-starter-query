ccf469a07df2fa234d3eb38b3a2670c67d031b11
Whitney O'Meara <james.w.omeara@gmail.com>
2024-09-22 01:50:59 +0000

    1.0.7

--- src/main/java/datawave/microservice/query/config/QueryStarterConfiguration.java
+++ src/main/java/datawave/microservice/query/config/QueryStarterConfiguration.java
@@ -1,7 +1,11 @@
 package datawave.microservice.query.config;
 
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import com.github.benmanes.caffeine.cache.CaffeineSpec;
 
 import datawave.microservice.query.edge.config.EdgeDictionaryProviderProperties;
 import datawave.microservice.query.mapreduce.config.MapReduceQueryProperties;
@@ -9,4 +13,12 @@ import datawave.microservice.query.stream.StreamingProperties;
 
 @Configuration
 @EnableConfigurationProperties({QueryProperties.class, MapReduceQueryProperties.class, StreamingProperties.class, EdgeDictionaryProviderProperties.class})
-public class QueryStarterConfiguration {}
+public class QueryStarterConfiguration {
+    
+    @Bean
+    public CaffeineCacheManager dateIndexHelperCacheManager() {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.setCaffeineSpec(CaffeineSpec.parse("maximumSize=1000, expireAfterAccess=24h, expireAfterWrite=24h"));
+        return caffeineCacheManager;
+    }
+}
